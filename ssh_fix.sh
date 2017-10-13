f87ab8001b4b8d28e6c1bf9229bc6965ef3a626e
Jing Shao <jing.shao@spirent.com>
2017-10-13 10:26:22 -0500

    Update

--- ssh_fix.sh
+++ ssh_fix.sh
@@ -1,4 +1,6 @@
 #!/bin/bash 
 
-pip install paramiko
-pip install --upgrade --user 'cryptography==1.9'
+sudo pip install paramiko
+sudo pip install --upgrade --user 'cryptography==1.9'
+sudo pip install configparser
+sudo pip install scp
