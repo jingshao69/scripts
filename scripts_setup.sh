6e1917deee21e80681e07f3655b3a500b176368d
Jing Shao <jing.shao@spirent.com>
2017-11-14 10:04:56 -0600

    Added scripts_setup.sh

---
+++ scripts_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+IFS=:
+
+SCRIPTS=docker_env.sh:install_lxc.sh:start_docker.sh:go_lxc.sh:ls_lxc.sh:start_lxc.sh:install_docker.sh:lxc_env.sh:stop_lxc.sh
+
+CWD=$(pwd)
+pushd $HOME/bin
+
+for i in ${SCRIPTS}
+do
+	if [ -e $CWD/$i ]; then
+		echo ln -sf $CWD/$i $i
+		ln -sf $CWD/$i $i
+	fi
+done
+
+popd
