6d3d1f300da83c9ebab73c48e698398f2ef8924d
Jing Shao <jing.shao@spirent.com>
2020-03-02 16:54:22 -0600

    Updated

--- scripts_setup.sh
+++ scripts_setup.sh
@@ -2,7 +2,7 @@
 
 IFS=:
 
-SCRIPTS=docker_env.sh:install_lxc.sh:start_docker.sh:go_lxc.sh:ls_lxc.sh:start_lxc.sh:install_docker.sh:lxc_env.sh:stop_lxc.sh:check_kmod.sh:check_kconfig.sh:check_pkg.sh
+SCRIPTS=docker_env.sh:install_lxc.sh:start_docker.sh:go_lxc.sh:ls_lxc.sh:start_lxc.sh:install_docker.sh:lxc_env.sh:stop_lxc.sh:check_kmod.sh:check_kconfig.sh:check_pkg.sh:jupyter.sh
 
 CWD=$(pwd)
 pushd $HOME/bin
