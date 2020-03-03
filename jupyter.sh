e912f2aa184bc42c5b14af46c3568722126cc9bd
Jing Shao <jing.shao@spirent.com>
2020-03-02 22:04:45 -0600

    Updated

--- jupyter.sh
+++ jupyter.sh
@@ -2,10 +2,23 @@
 
 case $1 in
 
-start)
+install)
+    echo sudo docker pull jupyter/scipy-notebook
+    sudo docker pull jupyter/scipy-notebook
+    ;;
+
+run)
     echo sudo docker run -p 8888:8888 jupyter/scipy-notebook
     sudo docker run -p 8888:8888 jupyter/scipy-notebook
     ;;
+
+start)
+    DOCKER_ID=$(sudo docker ps -a | grep jupyter | awk '{print $1}')
+    #DOCKED_ID=$(sudo docker ps | grep jupyter)
+    echo sudo docker start $DOCKER_ID
+    sudo docker start -ai $DOCKER_ID
+    ;;
+
 
 stop)
     DOCKER_ID=$(sudo docker ps | grep jupyter | awk '{print $1}')
